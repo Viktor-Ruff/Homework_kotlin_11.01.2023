4039e347e28cabcbcb90491a368813a42aa67b58
Viktor-Ruff <HamiltonE.T.O>
2023-01-11 11:48:41 +0400

    homework 1 is complete

---
+++ app/src/main/java/com/example/homework1/Weather.kt
@@ -0,0 +1,8 @@
+package com.example.homework1
+
+/**
+ * Created by Viktor-Ruff
+ * Date: 11.01.2023
+ * Time: 10:28
+ */
+data class Weather (var temperature: Int = 20, var typeWeather: String = "sunny")
